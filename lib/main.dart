2cad09721f344ea9a42916e0f2cc12e6e01e8c42
nuri <nuriddin@utec.kr>
2023-05-16 11:12:04 +0900

    TodayPage, HistoryPage, Method and Widget extraction.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'components/app_themes.dart';
+import 'package:medicalaid/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,63 +13,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      // theme: AppThemes.darkTheme,
       theme: AppThemes.lightTheme,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
       builder: (context, child) => MediaQuery(
         data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
         child: child!,
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 15,
-              ),
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,84 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:medicalaid/components/app_colors.dart';
+import 'package:medicalaid/pages/add/add_page.dart';
+import 'package:medicalaid/pages/history/history_page.dart';
+import 'package:medicalaid/pages/today/today_page.dart';
 
-class Homepage extends StatelessWidget {
-  const Homepage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _pages = [
+    const TodayPage(),
+    const HistoryPage(),
+  ];
+
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.white,
+      child: SafeArea(
+        top: false,
+        child: Scaffold(
+          appBar: AppBar(),
+          body: _pages[_currentIndex],
+          floatingActionButton: FloatingActionButton(
+            onPressed: _onAddMedicine,
+            child: const Icon(CupertinoIcons.add),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          bottomNavigationBar: _buildBottomAppBar(),
+        ),
+      ),
+    );
+  }
 
+  BottomAppBar _buildBottomAppBar() {
+    return BottomAppBar(
+          elevation: 0,
+          child: Container(
+            height: kBottomNavigationBarHeight,
+            color: Colors.white,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                CupertinoButton(
+                  onPressed: () => _onCurrentPage(0),
+                  child: Icon(
+                    CupertinoIcons.checkmark,
+                    color: _currentIndex == 0 ? AppColors.primaryColor : Colors.grey,
+                  ),
+                ),
+                CupertinoButton(
+                  onPressed: () => _onCurrentPage(1),
+                  child: Icon(
+                    CupertinoIcons.text_badge_checkmark,
+                    color: _currentIndex == 1 ? AppColors.primaryColor : Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+  }
+
+  void _onCurrentPage(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  void _onAddMedicine() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const AddPage()),
     );
   }
 }
