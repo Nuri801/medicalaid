22157893d63ebc8865fd4b385a9bde97b8ce094f
nuri <nuriddin@utec.kr>
2023-08-07 10:39:19 +0900

    Notification plugin added. Working on creating a notifications service class.

--- lib/components/app_constants.dart
+++ lib/components/app_constants.dart
@@ -13,3 +13,4 @@ const EdgeInsetsGeometry textFieldContentPadding = EdgeInsets.symmetric(horizont
 const EdgeInsetsGeometry pagePadding = EdgeInsets.all(20.0);
 
 const EdgeInsetsGeometry submitButtonPadding = EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0);
+
